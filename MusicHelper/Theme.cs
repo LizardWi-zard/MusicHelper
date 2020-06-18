821c234b4168e35a572d719531d60d3e2dc1a4ba
Lizard_W1zard <artemef1996@gmail.com>
2020-06-19 00:49:26 +0300

    abstract class added

--- MusicHelper/Theme.cs
+++ MusicHelper/Theme.cs
@@ -79,148 +79,102 @@ namespace MusicHelper
         }
 
         class SmallTheme : ThemeOptions
-        {           
-            Size WindowMinSize { get; set; } = new Size(250, 385);
+        {
+            public SmallTheme()
+            {
+                WindowMinSize = new Size(250, 385);
+                WindowMaxSize = new Size(250, 385);
 
-            Size WindowMaxSize { get; set; } = new Size(250, 385);
+                OpenButtonSize = new Size(97, 23);
+                OpenButtonLocation = new Point(125, 311);
 
+                musicListBoxSize = new Size(210, 121);
+                musicListBoxLocation = new Point(12, 184);
 
-            Size OpenButtonSize { get; set; } = new Size(97, 23);
+                MusicValueSize = new Size(210, 45);
+                MusicValueLocation = new Point(12, 67);
 
-            Point OpenButtonLocation { get; set; } = new Point(125, 311);
+                StartButtonSize = new Size(68, 25);
+                StartButtonLocation = new Point(83, 12);
 
+                PreviousTrackSize = new Size(65, 25);
+                PreviousTrackLocation = new Point(12, 12);
 
-            Size musicListBoxSize { get; set; } = new Size(210, 121);
+                NextTrackSize = new Size(65, 25);
+                NextTrackLocation = new Point(157, 12);
 
-            Point musicListBoxLocation { get; set; } = new Point(12, 184);
+                LeftTrackCountSize = new Size(65, 20);
 
+                LeftTrackCountLocation = new Point(12, 43);
 
-            Size MusicValueSize { get; set; } = new Size(210, 45);
+                InfinitiMusicSize = new Size(50, 17);
+                InfinitiMusicLocation = new Point(81, 44);
 
-            Point MusicValueLocation { get; set; } = new Point(12, 67);
+                RandomTrackSize = new Size(85, 17);
+                RandomTrackLocation = new Point(137, 44);
 
+                CurrentMomentLableSize = new Size(34, 13);
+                CurrentMomentLableLocation = new Point(97, 99);
 
-            Size StartButtonSize { get; set; } = new Size(68, 25);
+                Label1Size = new Size(34, 13);
+                Label1Location = new Point(12, 99);
 
-            Point StartButtonLocation { get; set; } = new Point(83, 12);
+                MaxLengthLabelSize = new Size(34, 13);
+                MaxLengthLabelLocation = new Point(188, 99);
 
-
-            Size PreviousTrackSize { get; set; } = new Size(65, 25);
-
-            Point PreviousTrackLocation { get; set; } = new Point(12, 12);
-
-
-            Size NextTrackSize { get; set; } = new Size(65, 25);
-
-            Point NextTrackLocation { get; set; } = new Point(157, 12);
-
-
-            Size LeftTrackCountSize { get; set; } = new Size(65, 20);
-
-            Point LeftTrackCountLocation { get; set; } = new Point(12, 43);
-
-
-            Size InfinitiMusicSize { get; set; } = new Size(50, 17);
-
-            Point InfinitiMusicLocation { get; set; } = new Point(81, 44);
-
-
-            Size RandomTrackSize { get; set; } = new Size(85, 17);
-
-            Point RandomTrackLocation { get; set; } = new Point(137, 44);
-
-
-            Size CurrentMomentLableSize { get; set; } = new Size(34, 13);
-
-            Point CurrentMomentLableLocation { get; set; } = new Point(97, 99);
-
-
-            Size Label1Size { get; set; } = new Size(34, 13);
-
-            Point Label1Location { get; set; } = new Point(12, 99);
-
-
-            Size MaxLengthLabelSize { get; set; } = new Size(34, 13);
-
-            Point MaxLengthLabelLocation { get; set; } = new Point(188, 99);
-
-
-            Size LoudTrackBarSize { get; set; } = new Size(210, 45);
-
-            Point LoudTrackBarLocation { get; set; } = new Point(12, 133);
+                LoudTrackBarSize = new Size(210, 45);
+                LoudTrackBarLocation = new Point(12, 133);
+            }
         }
 
 
 
         class BigTheme : ThemeOptions
         {
-            public Size WindowMinSize { get; set; } = new Size(750, 500);
+            public BigTheme()
+            {
+                WindowMinSize = new Size(750, 500);
+                WindowMaxSize  = new Size(750, 500);
 
-            public Size WindowMaxSize { get; set; } = new Size(750, 500);
+                OpenButtonSize = new Size(75, 45);
+                OpenButtonLocation  = new Point(647, 404);
 
+                musicListBoxSize  = new Size(113, 329);
+                musicListBoxLocation = new Point(609, 12);
 
-            public Size OpenButtonSize { get; set; } = new Size(75, 45);
+                MusicValueSize  = new Size(710, 45);
+                MusicValueLocation  = new Point(12, 353);
 
-            public Point OpenButtonLocation { get; set; } = new Point(647, 404);
+                StartButtonSize = new Size(28, 25);
+                StartButtonLocation = new Point(365, 424);
 
+                PreviousTrackSize  = new Size(28, 25);
+                PreviousTrackLocation  = new Point(331, 424);
 
-            public Size musicListBoxSize { get; set; } = new Size(113, 329);
+                NextTrackSize  = new Size(28, 25);
+                NextTrackLocation = new Point(399, 424);
 
-            public Point musicListBoxLocation { get; set; } = new Point(609, 12);
+                LeftTrackCountSize = new Size(42, 20);
+                LeftTrackCountLocation = new Point(283, 428);
 
+                InfinitiMusicSize  = new Size(32, 17);
+                InfinitiMusicLocation  = new Point(433, 429);
 
-            public Size MusicValueSize { get; set; } = new Size(710, 45);
+                RandomTrackSize= new Size(85, 17);
+                RandomTrackLocation = new Point(472, 429);
 
-            public Point MusicValueLocation { get; set; } = new Point(12, 353);
+                CurrentMomentLableSize  = new Size(34, 13);
+                CurrentMomentLableLocation = new Point(362, 401);
 
+                Label1Size = new Size(34, 13);
+                Label1Location  = new Point(12, 385);
 
-            public Size StartButtonSize { get; set; } = new Size(28, 25);
+                MaxLengthLabelSize = new Size(34, 13);
+                MaxLengthLabelLocation = new Point(686, 385);
 
-            public Point StartButtonLocation { get; set; } = new Point(365, 424);
-
-
-            public Size PreviousTrackSize { get; set; } = new Size(28, 25);
-
-            public Point PreviousTrackLocation { get; set; } = new Point(331, 424);
-
-
-            public Size NextTrackSize { get; set; } = new Size(28, 25);
-
-            public Point NextTrackLocation { get; set; } = new Point(399, 424);
-
-
-            public Size LeftTrackCountSize { get; set; } = new Size(42, 20);
-
-            public Point LeftTrackCountLocation { get; set; } = new Point(283, 428);
-
-
-            public Size InfinitiMusicSize { get; set; } = new Size(32, 17);
-
-            public Point InfinitiMusicLocation { get; set; } = new Point(433, 429);
-
-
-            public Size RandomTrackSize { get; set; } = new Size(85, 17);
-
-            public Point RandomTrackLocation { get; set; } = new Point(472, 429);
-
-
-            public Size CurrentMomentLableSize { get; set; } = new Size(34, 13);
-
-            public Point CurrentMomentLableLocation { get; set; } = new Point(362, 401);
-
-
-            public Size Label1Size { get; set; } = new Size(34, 13);
-
-            public Point Label1Location { get; set; } = new Point(12, 385);
-
-
-            public Size MaxLengthLabelSize { get; set; } = new Size(34, 13);
-
-            public Point MaxLengthLabelLocation { get; set; } = new Point(686, 385);
-
-            public Size LoudTrackBarSize { get; set; } = new Size(204, 45);
-
-            public Point LoudTrackBarLocation { get; set; } = new Point(12, 404);
+                LoudTrackBarSize = new Size(204, 45);
+                LoudTrackBarLocation = new Point(12, 404);
+            }
         }
 
        
